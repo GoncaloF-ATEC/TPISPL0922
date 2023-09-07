26311fcaed5b386731bd96f3c25a9b88a4317fcd
GoncaloF-ATEC <Goncalo.Feliciano.0001150@edu.atec.pt>
2023-09-07 17:08:20 +0100

    var

--- 5423/demo1.playground/Contents.swift
+++ 5423/demo1.playground/Contents.swift
@@ -1,3 +1,55 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var greeting = "Hello"
+
+print(greeting)
+
+
+//var
+
+var nome:Int = 10
+var nome2 = 12
+
+nome2 = 23
+// nome2 = "" -> error: demo1.playground:14:9: error: cannot assign value of type 'String' to type 'Int'
+
+var num1 = 10
+var num2 = 15
+var soma1 = num1 + num2
+
+var num3:Float = 10.5
+
+//var soma2 = num1 + num3 -> Binary operator '+' cannot be applied to operands of type 'Int' and 'Float'
+
+var soma2 = num3 + Float(num1)
+
+
+var num4 = 10.5
+
+// var soma2 = num4 + Float(num1) -> Binary operator '+' cannot be applied to operands of type 'Double' and 'Float'
+
+
+var res = 12.999999
+
+var res_int = Int(res)
+
+
+var π = 3.14
+var _dsdsad = 12
+var 🇵🇹 = "Portugal"
+var 🍏 = "teste"
+
+
+//const
+
+
+//operações
+
+
+// condições
+
+
+// loops
+
+
+//Funções
