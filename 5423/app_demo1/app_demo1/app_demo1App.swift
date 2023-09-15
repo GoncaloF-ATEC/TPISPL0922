ca19857c162679f0bdb74122762f1ce82bee1492
GoncaloF-ATEC <Goncalo.Feliciano.0001150@edu.atec.pt>
2023-09-15 16:44:11 +0100

    base app 1

---
+++ 5423/app_demo1/app_demo1/app_demo1App.swift
@@ -0,0 +1,17 @@
+//
+//  app_demo1App.swift
+//  app_demo1
+//
+//  Created by Gonçalo Feliciano on 15/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct app_demo1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
