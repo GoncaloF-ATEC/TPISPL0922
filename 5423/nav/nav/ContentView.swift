080413a92934bba73a414f326b77ae63b5cf2d6b
GoncaloF-ATEC <Goncalo.Feliciano.0001150@edu.atec.pt>
2023-09-21 16:33:25 +0100

    Nav base no xcode15

---
+++ 5423/nav/nav/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  nav
+//
+//  Created by Gonçalo Feliciano on 21/09/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+
+
+
+#Preview {
+    ContentView()
+}
---
+++ 5423/nav/nav/navApp.swift
@@ -0,0 +1,17 @@
+//
+//  navApp.swift
+//  nav
+//
+//  Created by Gonçalo Feliciano on 21/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct navApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
