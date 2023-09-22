6d04713a112f5a42a70cb594371606d618358170
GoncaloF-ATEC <Goncalo.Feliciano.0001150@edu.atec.pt>
2023-09-22 16:36:35 +0100

    grid_base

---
+++ 5423/grid/grid/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  grid
+//
+//  Created by Gonçalo Feliciano on 22/09/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        NavigationStack{
+            VStack{
+                
+                
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 5423/grid/grid/gridApp.swift
@@ -0,0 +1,17 @@
+//
+//  gridApp.swift
+//  grid
+//
+//  Created by Gonçalo Feliciano on 22/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct gridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
